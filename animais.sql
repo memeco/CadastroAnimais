116c71f547545d0ba9f2a5707084b01378d65fa6
Emerson Marques Pedro <emersonmp@gmail.com>
2024-06-14 20:07:34 -0300

    Adicionei .sql

---
+++ animais.sql
@@ -0,0 +1,16 @@
+-- Cria o banco de dados
+CREATE DATABASE db_animais;
+
+-- Seleciona o banco de dados
+USE db_animais;
+
+-- Cria a tabela animais
+CREATE TABLE animais (
+    ID_Animal INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(100) NOT NULL,
+    Especie VARCHAR(50) NOT NULL,
+    Raca VARCHAR(50) NOT NULL,
+    Idade INT NOT NULL,
+    Peso DECIMAL(5, 2) NOT NULL,
+    Cor VARCHAR(20) NOT NULL
+)
